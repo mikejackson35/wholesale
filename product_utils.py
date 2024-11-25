1a83bd2e17744bd0d95c57a1a47085fbba60787f
Mike_Jackson <mikejackson35@gmail.com>
2024-11-25 12:35:09 -0800

    update

--- product_utils.py
+++ product_utils.py
@@ -12,48 +12,48 @@ products = [
         },
     {
         "name": "Chicken & Waffles",
-        "image_225": "assets\Wilde_1oz_8ct_Carton_CW_06012023.png",
-        "image_134": "assets\Wilde_Rendering_12ct_CW_09152022.png",
+        "image_225": "assets/Wilde_1oz_8ct_Carton_CW_06012023.png",
+        "image_134": "assets/Wilde_Rendering_12ct_CW_09152022.png",
         "description": "Description Here", 
         "price_134": 13.60,
         "price_225": 37.20
         },
     {
         "name": "BBQ", 
-        "image_225": "assets\Wilde_1oz_8ct_Carton_BBQ_06012023.png",
-        "image_134": "assets\Wilde_Rendering_12ct_BBQ_02292024.png",
+        "image_225": "assets/Wilde_1oz_8ct_Carton_BBQ_06012023.png",
+        "image_134": "assets/Wilde_Rendering_12ct_BBQ_02292024.png",
         "description": "Description Here", 
         "price_134": 13.60,
         "price_225": 37.20
         },
     {
         "name": "Sea Salt Vinegar", 
-        "image_225": "assets\Wilde_1oz_8ct_Carton_SSV_06012023.png",
-        "image_134": "assets\Wilde_Rendering_12ct_SSV_09152022.png",
+        "image_225": "assets/Wilde_1oz_8ct_Carton_SSV_06012023.png",
+        "image_134": "assets/Wilde_Rendering_12ct_SSV_09152022.png",
         "description": "Description Here", 
         "price_134": 13.60,
         "price_225": 37.20
         },
     {
         "name": "Himalayan Pink Salt", 
-        "image_225": "assets\Wilde_1oz_8ct_Carton_Pink_06012023.png",
-        "image_134": "assets\Wilde_Rendering_12ct_Pink_09152022.png", 
+        "image_225": "assets/Wilde_1oz_8ct_Carton_Pink_06012023.png",
+        "image_134": "assets/Wilde_Rendering_12ct_Pink_09152022.png", 
         "description": "Description Here", 
         "price_134": 13.60,
         "price_225": 37.20
         },
     {
         "name": "Spicy Queso", 
-        "image_225": "assets\Wilde_1oz_8ct_Carton_SQ_12052023.png",
-        "image_134": "assets\Wilde_Rendering_12ct_SQ_04052024.png",
+        "image_225": "assets/Wilde_1oz_8ct_Carton_SQ_12052023.png",
+        "image_134": "assets/Wilde_Rendering_12ct_SQ_04052024.png",
         "description": "Description Here", 
         "price_134": 13.60,
         "price_225": 37.20
         },
     {
         "name": "Nashville Hot", 
-        "image_225": "assets\Wilde_1oz_8ct_Carton_Nashville_06012023.png",
-        "image_134": "assets\Wilde_Rendering_12ct_NH_04082024.png",
+        "image_225": "assets/Wilde_1oz_8ct_Carton_Nashville_06012023.png",
+        "image_134": "assets/Wilde_Rendering_12ct_NH_04082024.png",
         "description": "Description Here", 
         "price_134": 13.60,
         "price_225": 37.20
