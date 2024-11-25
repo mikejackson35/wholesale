ee985d910324bec1947edd2a093c9568b7c1d5e1
Mike_Jackson <mikejackson35@gmail.com>
2024-11-25 09:56:28 -0800

    updatre

--- app.py
+++ app.py
@@ -1,60 +1,8 @@
 import streamlit as st
+from product_utils import products
 
 # Set the page layout to wide mode
 st.set_page_config(layout="wide")
-
-# Define product details
-products = [
-    {
-        "name": "Buffalo", 
-        "main_image": "assets/Wilde_1.34oz_Buffalo_WithoutShadow_02242023.png",
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-    {
-        "name": "Chicken & Waffles",
-        "main_image": "assets/Wilde_1.34oz_CW_WithoutShadow_02242023.png",
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-    {
-        "name": "BBQ", 
-        "main_image": "assets/Wilde_1.34oz_BBQ_WithoutShadow_02242023.png", 
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-    {
-        "name": "Sea Salt Vinegar", 
-        "main_image": "assets/Wilde_1.34oz_SSV_WithoutShadow_02242023.png", 
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-    {
-        "name": "Himalayan Pink Salt", 
-        "main_image": "assets/Wilde_1.34oz_Pink_WithoutShadow_02242023.png", 
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-    {
-        "name": "Spicy Queso", 
-        "main_image": "assets/Wilde_1.34oz_NashvilleHot_WithoutShadow_02242023.png", 
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-    {
-        "name": "Nashville Hot", 
-        "main_image": "assets/Wilde_1.34oz_NashvilleHot_WithoutShadow_02242023.png", 
-        "description": "Description Here", 
-        "price_134": 13.60,
-        "price_225": 37.20
-        },
-]
 
 # Initialize order summary state
 if "order_summary" not in st.session_state:
---
+++ product_utils.py
@@ -0,0 +1,52 @@
+# Define product details
+products = [
+    {
+        "name": "Buffalo", 
+        "main_image": "assets/Wilde_1.34oz_Buffalo_WithoutShadow_02242023.png",
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+    {
+        "name": "Chicken & Waffles",
+        "main_image": "assets/Wilde_1.34oz_CW_WithoutShadow_02242023.png",
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+    {
+        "name": "BBQ", 
+        "main_image": "assets/Wilde_1.34oz_BBQ_WithoutShadow_02242023.png", 
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+    {
+        "name": "Sea Salt Vinegar", 
+        "main_image": "assets/Wilde_1.34oz_SSV_WithoutShadow_02242023.png", 
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+    {
+        "name": "Himalayan Pink Salt", 
+        "main_image": "assets/Wilde_1.34oz_Pink_WithoutShadow_02242023.png", 
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+    {
+        "name": "Spicy Queso", 
+        "main_image": "assets/Wilde_1.34oz_NashvilleHot_WithoutShadow_02242023.png", 
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+    {
+        "name": "Nashville Hot", 
+        "main_image": "assets/Wilde_1.34oz_NashvilleHot_WithoutShadow_02242023.png", 
+        "description": "Description Here", 
+        "price_134": 13.60,
+        "price_225": 37.20
+        },
+]
